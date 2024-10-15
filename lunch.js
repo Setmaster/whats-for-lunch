0010de3eddd9f2c98401910da9cef4b6a6a5cdbb
Vitor <vi7org@hotmail.com>
2024-10-14 21:12:29 -0400

    Fix eslint issues

--- lunch.js
+++ lunch.js
@@ -14,19 +14,16 @@
  */
 
 const whatToDoForLunch = function(hungry, availableTime) {
-    if (hungry === false){
-        console.log("I need to get back to work.");
-    }
-    else if (availableTime < 20){
-        console.log("I'll pick something up and eat it in the lab.");
-    }
-    else if (availableTime >= 20 && availableTime <= 30){
-        console.log("I'll try a place nearby.");
-    }
-    else {
-        console.log("I'm in a bootcamp and I should reconsider how much time I actually have to spare.");
-    }
-}
+  if (hungry === false) {
+    console.log("I need to get back to work.");
+  } else if (availableTime < 20) {
+    console.log("I'll pick something up and eat it in the lab.");
+  } else if (availableTime >= 20 && availableTime <= 30) {
+    console.log("I'll try a place nearby.");
+  } else {
+    console.log("I'm in a bootcamp and I should reconsider how much time I actually have to spare.");
+  }
+};
 
 
 /*
